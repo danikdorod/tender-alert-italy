01111d84e0092ebf96a4ad51e205b3726fa99b46
danikdorod <danikdorod@gmail.com>
2026-04-03 17:02:25 +0200

    Update fetch_tenders.py

--- scripts/fetch_tenders.py
+++ scripts/fetch_tenders.py
@@ -4,7 +4,6 @@ url = "https://api.ted.europa.eu/v3/notices/search"
 
 payload = {
     "page": 1,
-    "pageSize": 20,
     "fields": [
         "OPP-021-Contract",   # contract ID
         "BT-13(t)-Part"       # title
